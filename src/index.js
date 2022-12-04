afaff24f3f7f41a01e3271ee0ea7adf9ba359981
Collins Chirchir <collinskchirchir@gmail.com>
2022-12-04 13:29:58 +0300

    Added Toy Info to Card

--- src/index.js
+++ src/index.js
@@ -13,3 +13,55 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Fetch Data from JSON db.json
+function fetchToys()
+{
+  return fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(data => renderToys(data))
+}
+
+// Add each toy to card div``
+function renderToys(toys)
+{
+  const toyCollection = document.querySelector('#toy-collection')
+  toys.forEach(toy => {
+    // Create Toy Name  Attribute
+    const h2 = document.createElement('h2')
+    h2.textContent = toy.name
+    
+    // Create Image Section
+    const img = document.createElement('img');
+    img.src = toy.image;
+    img.setAttribute('class', 'toy-avatar');
+
+    // Create Like Section
+    const like = document.createElement('p')
+    like.textContent = `${toy.likes} Likes`;
+    
+    // Create Button
+    const btn = document.createElement('button');
+    btn.setAttribute('class', 'like-btn');
+    btn.setAttribute('id', `${toy.id}`);
+    btn.textContent = 'Like ❤️'
+    
+    // Add to Card Items
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+    card.appendChild(h2)
+    card.appendChild(img)
+    card.appendChild(like)
+    card.appendChild(btn)
+    
+    // Add to container
+    toyCollection.appendChild(card)
+  })
+  // console.log(toys)
+}
+
+
+
+document.addEventListener('DOMContentLoaded',function()
+{
+  fetchToys()})
